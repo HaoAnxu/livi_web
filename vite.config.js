bb4688fea39ca681abc99095afe6b2d5ec615c6e
郝安旭 <haoanxu@neusoft.com>
2025-11-18 09:02:12 +0800

    有请求了

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,13 @@ export default defineConfig({
                 secure: false,
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+            '/ws': { // 匹配以 /ws 开头的路径
+                target: 'ws://localhost:8080', // 注意：这里是 ws:// 协议
+                secure: false,
+                changeOrigin: true,
+                ws: true, // 关键：开启 WebSocket 代理
+                rewrite: (path) => path // 不用重写路径
             }
         }
     }
